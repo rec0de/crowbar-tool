6208e2c929d21be2b0d6e0f4e075f811984b7053
rec0de <mail@rec0de.net>
2020-05-16 22:33:32 +0200

    Proper indentation of testcases, slighly neater location rendering

--- src/main/kotlin/org/abs_models/crowbar/investigator/NodeInfoRenderer.kt
+++ src/main/kotlin/org/abs_models/crowbar/investigator/NodeInfoRenderer.kt
@@ -1,5 +1,8 @@
 package org.abs_models.crowbar.investigator
 
+import org.abs_models.crowbar.data.Location
+import org.abs_models.crowbar.data.Field
+import org.abs_models.crowbar.data.ProgVar
 import org.abs_models.crowbar.tree.NodeInfo
 import org.abs_models.crowbar.tree.NodeInfoVisitor
 import org.abs_models.crowbar.tree.NoInfo
@@ -19,41 +22,78 @@ import org.abs_models.crowbar.tree.InfoSkip
 
 object NodeInfoRenderer: NodeInfoVisitor<String> {
 
-    override fun visit(info: InfoAwaitUse) = "await ${info.guard.prettyPrint()};"
+    private var indentLevel = 0
+    private var indentString = "\t"
+
+    override fun visit(info: InfoAwaitUse) = indent("await ${info.guard.prettyPrint()};")
 
     override fun visit(info: InfoClassPrecondition) = ""
 
-    override fun visit(info: InfoIfElse) = "if(${info.guard.prettyPrint()}){}\nelse{"
+    override fun visit(info: InfoIfElse): String {
+        indentLevel -= 1
+        return indent("if(${info.guard.prettyPrint()}){}\nelse{")
+    }
 
-    override fun visit(info: InfoIfThen) = "if(${info.guard.prettyPrint()}){"
+    override fun visit(info: InfoIfThen): String {
+        indentLevel -= 1
+        return indent("if(${info.guard.prettyPrint()}){")
+    }
 
     override fun visit(info: InfoInvariant) = ""
 
-    override fun visit(info: InfoLocAssign) = "${info.lhs.prettyPrint()} = ${info.expression.prettyPrint()};"
+    override fun visit(info: InfoLocAssign): String {
+        val location = renderLocation(info.lhs)
+        return indent("$location = ${info.expression.prettyPrint()};")
+    }
 
-    override fun visit(info: InfoLoopInitial) = "while(${info.guard.prettyPrint()}) { }"
+    override fun visit(info: InfoLoopInitial) = indent("while(${info.guard.prettyPrint()}) { }")
 
     override fun visit(info: InfoLoopPreserves): String {
-        return "// Known true:\n" +
-               "// Loop guard: ${info.guard.prettyPrint()}\n" +
-               "// Loop invariant: ${info.loopInv.prettyPrint()}\n" +
-               "while(${info.guard.prettyPrint()}) {"
+        indentLevel -= 1
+
+        val text = "// Known true:\n" +
+                   "// Loop guard: ${info.guard.prettyPrint()}\n" +
+                   "// Loop invariant: ${info.loopInv.prettyPrint()}\n" +
+                   "while(${info.guard.prettyPrint()}) {"
+
+        return indent(text)
     }
 
     override fun visit(info: InfoLoopUse): String {
-        return "while(${info.guard.prettyPrint()}){ ... } \n" +
-               "// Known true:\n" + 
-               "// Negated loop guard: !(${info.guard.prettyPrint()})\n" +
-               "// Loop invariant: ${info.invariant.prettyPrint()}"
+        val text = "while(${info.guard.prettyPrint()}){ ... } \n" +
+                   "// Known true:\n" + 
+                   "// Negated loop guard: !(${info.guard.prettyPrint()})\n" +
+                   "// Loop invariant: ${info.invariant.prettyPrint()}"
+
+        return indent(text)
     }
 
-    override fun visit(info: InfoObjAlloc) = "${info.lhs.prettyPrint()} = new ${info.classInit.prettyPrint()};"
+    override fun visit(info: InfoObjAlloc) = indent("${renderLocation(info.lhs)} = new ${info.classInit.prettyPrint()};")
 
-    override fun visit(info: InfoReturn) = "return ${info.expression.prettyPrint()};"
+    override fun visit(info: InfoReturn) = indent("return ${info.expression.prettyPrint()};")
 
-    override fun visit(info: InfoScopeClose) = "}"
+    override fun visit(info: InfoScopeClose): String {
+        val text = indent("}")
+        indentLevel += 1
+        return text
+    }
 
     override fun visit(info: InfoSkip) = ""
 
-    override fun visit(info: NoInfo) = "[unknown rule application]"
+    override fun visit(info: NoInfo) = indent("[unknown rule application]")
+
+    private fun renderLocation(loc: Location): String {
+        return when(loc) {
+            is ProgVar -> loc.name
+            is Field -> "this.${loc.name}"
+            else -> loc.prettyPrint()
+        }
+    }
+
+    private fun indent(text: String): String {
+        val lines = text.split("\n")
+        val spacer = indentString.repeat(indentLevel)
+
+        return lines.map{ "$spacer$it" }.joinToString("\n")
+    }
 }
