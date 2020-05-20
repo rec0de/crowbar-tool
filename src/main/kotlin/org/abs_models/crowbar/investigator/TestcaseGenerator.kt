e11af1ef6f432493f5fa694b3796b7b58b65e514
rec0de <mail@rec0de.net>
2020-05-20 14:13:08 +0200

    Add proof obligation info to testcases

--- src/main/kotlin/org/abs_models/crowbar/investigator/TestcaseGenerator.kt
+++ src/main/kotlin/org/abs_models/crowbar/investigator/TestcaseGenerator.kt
@@ -4,33 +4,27 @@ import org.abs_models.crowbar.tree.LogicNode
 import org.abs_models.crowbar.tree.SymbolicNode
 import org.abs_models.crowbar.tree.SymbolicTree
 import org.abs_models.crowbar.tree.InfoNode
+import org.abs_models.crowbar.tree.LeafInfo
 import org.abs_models.crowbar.main.output
 import org.abs_models.crowbar.main.tmpPath
 import org.abs_models.crowbar.interfaces.plainSMTCommand
 import org.abs_models.crowbar.interfaces.generateSMT
+import org.abs_models.crowbar.data.Formula
 import java.io.File
 
 object TestcaseGenerator {
 	fun investigate(node: SymbolicNode) {
 
-		// Create child-parent mapping for symbolic tree
-		val parents = mutableMapOf<SymbolicTree,SymbolicTree?>()
-		val worklist = mutableListOf<SymbolicTree>(node)
-
-		while(worklist.isNotEmpty()) {
-			val elem = worklist.removeAt(0)
-
-			if(elem is SymbolicNode) {
-				elem.children.forEach {
-					parents[it] = elem
-				}
-				worklist.addAll(elem.children)
-			}	
-		}
-
+		val parents = parentMapping(node)
 		val uncloseable = node.collectLeaves().first{ it is LogicNode && !it.evaluate() }
 
+		if(uncloseable !is InfoNode || uncloseable.info !is LeafInfo)
+			throw Exception("Unclodes branch does not have proof obligation information")
+
+		val obligations = (uncloseable.info as LeafInfo).obligations
+
 		output("Investigator: found unclosed branch")
+
 		output("Investigator: traversing upwards....")
 
 		val branchNodes = mutableListOf<SymbolicTree>()
@@ -56,11 +50,30 @@ object TestcaseGenerator {
 			}
 		}
 
-		output(statements.joinToString("\n"))
+		output(buildTestcase(statements, obligations))
 
 		output("Investigator: parsing model....")
 		getModel(uncloseable as LogicNode)
 	}	
+
+	fun parentMapping(root: SymbolicNode): Map<SymbolicTree,SymbolicTree?> {
+		// Create child-parent mapping for symbolic tree
+		val parents = mutableMapOf<SymbolicTree,SymbolicTree?>()
+		val worklist = mutableListOf<SymbolicTree>(root)
+
+		while(worklist.isNotEmpty()) {
+			val elem = worklist.removeAt(0)
+
+			if(elem is SymbolicNode) {
+				elem.children.forEach {
+					parents[it] = elem
+				}
+				worklist.addAll(elem.children)
+			}	
+		}
+
+		return parents
+	}
 
 	fun getModel(leaf: LogicNode) {
 		val smtRep = generateSMT(leaf.ante, leaf.succ, model = true)
@@ -72,5 +85,13 @@ object TestcaseGenerator {
 			val parsed = ModelParser.parse(model)
 			output(parsed.joinToString("\n"))
 		}	
+	}
+
+	fun buildTestcase(statements: List<String>, obligations: List<Pair<String,Formula>>): String {
+		val oblString = obligations.map{ "// ${it.first}: ${it.second.prettyPrint()}" }.joinToString("\n")
+		val stmtString = statements.joinToString("\n")
+		val explainer = "\n// Proof failed here. Trying to show:\n"
+
+		return stmtString + explainer + oblString
 	}
 }
