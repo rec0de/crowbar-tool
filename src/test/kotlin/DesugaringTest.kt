f2531c3b9a8f1d1bb687655a4918f92cfb031f84
MarcoScaletta <scalettamarco@yahoo.it>
2020-06-23 20:48:00 +0200

    Add tests for desugaring

---
+++ src/test/kotlin/DesugaringTest.kt
@@ -0,0 +1,30 @@
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import org.abs_models.crowbar.main.*
+import org.abs_models.crowbar.types.PostInvType
+import java.nio.file.Paths
+
+class DesugaringTest : StringSpec({
+    val postInv = PostInvType::class
+    for (smt in listOf("z3", "cvc")) {
+        println("testing with: $smt as backend")
+        smtPath = smt
+        "$smt desugaring"{
+            val (model, repos) = load(listOf(Paths.get("src/test/resources/desugaring.abs")))
+            val classDecl = model.extractClassDecl("Desugaring", "DesugaringC", repos)
+
+            val simpleDesugaringSuccess = classDecl.extractMethodNode(postInv,"simpleDesugaringSuccess", repos)
+            executeNode(simpleDesugaringSuccess, repos, postInv) shouldBe true
+
+            val simpleDesugaringFail = classDecl.extractMethodNode(postInv,"simpleDesugaringFail", repos)
+            executeNode(simpleDesugaringFail, repos, postInv) shouldBe false
+
+            val desugaringPartiallyInheritedFail = classDecl.extractMethodNode(postInv,"desugaringPartiallyInheritedFail", repos)
+            executeNode(desugaringPartiallyInheritedFail, repos, postInv) shouldBe false
+
+            val desugaringFullyInheritedFail = classDecl.extractMethodNode(postInv,"desugaringFullyInheritedFail", repos)
+            executeNode(desugaringFullyInheritedFail, repos, postInv) shouldBe true
+        }
+
+    }
+})
